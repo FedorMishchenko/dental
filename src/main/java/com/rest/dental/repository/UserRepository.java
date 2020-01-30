15d674fbaabfea1d8d3af6f1b529f61ac2a35fb9
Fedor Mishchenko <fedormishchenko.ua@gmail.com>
2020-01-30 23:07:59 +0200

    commit 2

--- src/main/java/com/rest/dental/repository/UserRepository.java
+++ src/main/java/com/rest/dental/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.rest.dental.repository;
 
 import com.rest.dental.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
